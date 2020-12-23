9c537e17933ebcf0353dc1178512eb5f42fcb5aa
mnk <wccf19900910@yahoo.co.jp>
2020-12-23 16:10:33 +0900

    雀士を表すクラスの定義

--- header/Mahjong.h
+++ header/Mahjong.h
@@ -10,3 +10,26 @@ class Mahjong {
 };
 
 string setResult_func(int number);// 満貫、跳満等の結果を設定するメンバ関数の実装
+
+// 雀士を表すクラスの定義
+class Sparrow {
+    protected:
+        string name;	// 雀士の名前を表すメンバ変数
+    public:
+        virtual void voice() = 0;   // 雀士の名前とボイスを表示するメンバ関数
+        Sparrow(string name); // コンストラクタ
+}
+
+// 麻雀格闘倶楽部を表すクラスの定義
+class MFC : public Sparrow {
+    public:
+        void voice();   // 雀士の名前とボイスを表示するメンバ関数
+        MFC(string name); // コンストラクタ
+}
+
+// サクラナイツを表すクラスの定義
+class Sakura : public Sparrow {
+    public:
+        void voice();   // 雀士の名前とボイスを表示するメンバ関数
+        Sakura(string name); // コンストラクタ
+}
